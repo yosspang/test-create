618c8103779df2d2b44ed0758ff5093e61159d8c
Yosua <yosspang95@gmail.com>
2019-09-23 14:09:13 +0700

    test commit php

---
+++ testphp.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Test PHP page</h1>
+
+<?php
+echo "Hello World! "; //sample comment
+/*$num1=1;
+$num2=5;
+$num3=2;
+$num4=10;
+$count=$num1+$num2*$num3/$num4;
+echo $num4;*/
+?> 
+
+</body>
+</html>
